006a2bcdbf6c218db0f8928ed312fb9b3e5a4b62
BillardVincent <59728481+BillardVincent@users.noreply.github.com>
2021-11-29 13:02:30 +0100

    en cours

--- src/main/java/fr/vbillard/tissusdeprincesseboot/controlers_v2/patron/PatronDetailController.java
+++ src/main/java/fr/vbillard/tissusdeprincesseboot/controlers_v2/patron/PatronDetailController.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PatronDetailController {
+	
+	/*
+	txtInput.setEditable(false);
+	txtInput.setMouseTransparent(true);
+	txtInput.setFocusTraversable(false);
+	*/
 }
