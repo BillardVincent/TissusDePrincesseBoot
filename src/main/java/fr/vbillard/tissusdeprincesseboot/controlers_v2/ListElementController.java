b9ae51c718ebbfbd60e2845dfbf7165aea6d73c2
vbillard <billard.vincent@gmail.com>
2021-12-28 22:41:53 +0100

    patron edit

--- src/main/java/fr/vbillard/tissusdeprincesseboot/controlers_v2/ListElementController.java
+++ src/main/java/fr/vbillard/tissusdeprincesseboot/controlers_v2/ListElementController.java
@@ -1,5 +1,6 @@
 package fr.vbillard.tissusdeprincesseboot.controlers_v2;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
@@ -15,19 +16,40 @@ import javafx.scene.layout.TilePane;
 @Component
 public class ListElementController implements IController{
 
+    @FXML
+    private Label ref;
+    @FXML
+    private Label titre;
+    @FXML
+    private Label description;
+    @FXML
+    private Label dimensions;
+
     protected StageInitializer initializer;
+
+    private ModelMapper mapper;
+
+    private ListElement element;
+
+    public ListElementController (ModelMapper mapper){
+        this.mapper = mapper;
+    }
     
 	protected void setElements() {
-		
+		ref.setText(element.getReference());
+        titre.setText(element.getTitre());
+        description.setText(element.getDescription());
+        dimensions.setText(element.getDimensions());
 	}
 	
-	ListElement element;
-	
+
 	
 	
     @Override
     public void setStageInitializer(StageInitializer initializer, Object... data) {
         this.initializer = initializer;
+        element = mapper.map(data[0], ListElement.class);
         setElements();
+
     }
 }
