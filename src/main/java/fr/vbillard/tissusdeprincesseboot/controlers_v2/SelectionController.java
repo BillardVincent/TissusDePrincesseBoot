96c4290405f4d4ef39351ad920a95c5f23369479
BillardVincent <59728481+BillardVincent@users.noreply.github.com>
2022-01-19 21:30:44 +0100

    Avancement tissu requis

---
+++ src/main/java/fr/vbillard/tissusdeprincesseboot/controlers_v2/SelectionController.java
@@ -0,0 +1,70 @@
+package fr.vbillard.tissusdeprincesseboot.controlers_v2;
+
+import org.springframework.stereotype.Component;
+
+import fr.vbillard.tissusdeprincesseboot.StageInitializer;
+import fr.vbillard.tissusdeprincesseboot.dtosFx.ProjetDto;
+import fr.vbillard.tissusdeprincesseboot.dtosFx.TissuRequisDto;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+@Component
+public class SelectionController implements IController {
+
+	@FXML
+	private Label descriptionProjetPanLabel;
+	@FXML
+	private Label marqueProjetPanLabel;
+	@FXML
+	private Label modelProjePantLabel;
+	@FXML
+	private Label typeVetementPanProjetLabel;
+
+	private StageInitializer initializer;
+	private ProjetDto projet;
+	private TissuRequisDto tissuRequis;
+
+	@Override
+	public void setStageInitializer(StageInitializer initializer, Object... data) {
+		this.initializer = initializer;
+		if (data.length == 2) {
+			if (data[0] instanceof ProjetDto) {
+				projet = (ProjetDto) data[0];
+				if (projet != null) {
+					descriptionProjetPanLabel.setText(projet.getDescription());
+					marqueProjetPanLabel.setText(projet.getPatron().getMarque());
+					modelProjePantLabel.setText(projet.getPatron().getModele());
+					typeVetementPanProjetLabel.setText(projet.getPatron().getTypeVetement());
+
+				} else {
+					descriptionProjetPanLabel.setText("");
+					marqueProjetPanLabel.setText("");
+					modelProjePantLabel.setText("");
+					typeVetementPanProjetLabel.setText("");
+				}
+
+				if (data[0] instanceof TissuRequisDto) {
+					tissuRequis = (TissuRequisDto) data[1];
+					if (projet != null) {
+						descriptionProjetPanLabel.setText(projet.getDescription());
+						marqueProjetPanLabel.setText(projet.getPatron().getMarque());
+						modelProjePantLabel.setText(projet.getPatron().getModele());
+						typeVetementPanProjetLabel.setText(projet.getPatron().getTypeVetement());
+
+					} else {
+						descriptionProjetPanLabel.setText("");
+						marqueProjetPanLabel.setText("");
+						modelProjePantLabel.setText("");
+						typeVetementPanProjetLabel.setText("");
+					}
+				}
+			}
+		}
+		setPane();
+	}
+
+	private void setPane() {
+		// TODO Auto-generated method stub
+		
+	}
+}
